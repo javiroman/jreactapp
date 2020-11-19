a8988c1aab746b86d8f3980e17af594799adb6f4
Javi Roman <jroman.espinar@gmail.com>
2020-11-19 11:32:35 +0100

    multimodule assembly working

--- jreactapp-bootstrap/src/main/java/org/jreactapp/RunJreactApp.java
+++ jreactapp-bootstrap/src/main/java/org/jreactapp/RunJreactApp.java
@@ -4,10 +4,8 @@ package org.jreactapp;
  * Hello world!
  *
  */
-public class RunJreactApp
-{
-    public static void main( String[] args )
-    {
+public class RunJreactApp {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
