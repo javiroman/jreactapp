490cc4d4193c94c3dcd0f7c542e12a1c8133bff4
Javi Roman <jroman.espinar@gmail.com>
2020-11-19 08:41:26 +0100

    working multi module structure

---
+++ jreactapp-bootstrap/src/main/java/org/jreactapp/RunJreactApp.java
@@ -0,0 +1,13 @@
+package org.jreactapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class RunJreactApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
