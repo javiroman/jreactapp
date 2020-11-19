8c353bb392717469744875e8a0df4a84b77b6961
Javi Roman <jroman.espinar@gmail.com>
2020-11-19 17:34:20 +0100

    simple jetty server

--- jreactapp-bootstrap/src/main/java/org/jreactapp/bootstrap/RunJreactApp.java
+++ jreactapp-bootstrap/src/main/java/org/jreactapp/bootstrap/RunJreactApp.java
@@ -1,14 +1,39 @@
 package org.jreactapp.bootstrap;
 
 import org.jreactapp.api.Api;
+import java.util.logging.Logger;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
 
 /**
- * Hello world!
- *
+ * Application entry point
  */
 public class RunJreactApp {
-    public static void main( String[] args ) {
+    private static final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+    private static Server createServer(final int port, final String contextPath){
+        Server server = new Server(port);
+
+        ContextHandler context = new ContextHandler();
+        context.setContextPath(contextPath);
+        context.setHandler(new ContainerHandler());
+
+        server.setHandler(context);
+
+        return server;
+    }
+
+    public static void main( String[] args ) throws Exception {
+
+
         Api app = new Api();
         app.greeting();
+
+        logger.info("Starting App Container");
+        Server server = createServer(8888, "/");
+
+        server.start();
+        server.join();
     }
 }
