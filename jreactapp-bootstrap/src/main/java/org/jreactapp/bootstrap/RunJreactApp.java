e27ec8007a92af97323bb39052e4e5ef6fede742
Javi Roman <jroman.espinar@gmail.com>
2020-11-20 07:33:52 +0100

    not working yet serving static content

--- jreactapp-bootstrap/src/main/java/org/jreactapp/bootstrap/RunJreactApp.java
+++ jreactapp-bootstrap/src/main/java/org/jreactapp/bootstrap/RunJreactApp.java
@@ -1,10 +1,17 @@
 package org.jreactapp.bootstrap;
 
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.util.resource.PathResource;
 import org.jreactapp.api.Api;
 import java.util.logging.Logger;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.DefaultHandler;
 
 /**
  * Application entry point
@@ -15,11 +22,24 @@ public class RunJreactApp {
     private static Server createServer(final int port, final String contextPath){
         Server server = new Server(port);
 
-        ContextHandler context = new ContextHandler();
-        context.setContextPath(contextPath);
-        context.setHandler(new ContainerHandler());
+        // Create the ResourceHandler. It is the object that will actually handle
+        // the request for a given file. It is a Jetty Handler object so it is
+        // suitable for chaining with other handlers.
+        ResourceHandler resourceHandler = new ResourceHandler();
 
-        server.setHandler(context);
+        // Configure the ResourceHandler. Setting the resource base indicates
+        // where the files should be served out of.
+        resourceHandler.setDirectoriesListed(true);
+        resourceHandler.setWelcomeFiles(new String[]{"index.html"});
+
+        Path userDir = Paths.get(System.getProperty("user.dir"));
+        PathResource pathResource = new PathResource(userDir);
+        resourceHandler.setBaseResource(pathResource);
+
+        // Add the ResourceHandler to the server.
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resourceHandler, new DefaultHandler()});
+        server.setHandler(handlers);
 
         return server;
     }
