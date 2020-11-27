332354cb81627e6c45e61addc48c69315feaaf86
Javi Roman <jroman.espinar@gmail.com>
2020-11-27 16:07:41 +0100

    working example

--- jreactapp-bootstrap/src/main/java/org/jreactapp/bootstrap/RunJreactApp.java
+++ jreactapp-bootstrap/src/main/java/org/jreactapp/bootstrap/RunJreactApp.java
@@ -1,17 +1,17 @@
 package org.jreactapp.bootstrap;
 
-import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import org.glassfish.jersey.server.ServerProperties;
 import org.jreactapp.api.Api;
 
 import java.net.MalformedURLException;
 import java.util.logging.Logger;
 
 import org.eclipse.jetty.server.Server;
+import org.jreactapp.api.JerseyService;
 
 /**
  * Application entry point
@@ -21,30 +21,33 @@ public class RunJreactApp {
 
     private static Server createServer(final int port, final String contextPath)
             throws MalformedURLException {
-
         System.setProperty("org.eclipse.jetty.LEVEL","DEBUG");
-        Server server = new Server(port);
-        String pwdPath = System.getProperty("user.dir");
 
-        ServletContextHandler staticContext = new ServletContextHandler();
-        staticContext.setContextPath("/");
+        Server server = new Server(port);
+
+        ServletContextHandler contextHandler = new ServletContextHandler();
+        contextHandler.setContextPath("/");
+
+        ServletHolder jerseyServlet = contextHandler.addServlet(
+                org.glassfish.jersey.servlet.ServletContainer.class, "/api/*");
+        //contextHandler.setInitParameter(ServerProperties.PROVIDER_CLASSNAMES, "org.jree");
+
+        jerseyServlet.setInitParameter( "jersey.config.server.provider.classnames",
+                JerseyService.class.getCanonicalName());
+
+        jerseyServlet.setInitOrder(0);
+
 
         ServletHolder staticHolder = new ServletHolder(new DefaultServlet());
         staticHolder.setInitParameter("resourceBase", "./lib/ui");
-        staticHolder.setInitParameter("pathInfoOnly", "true");
-        staticContext.addServlet(staticHolder, "/*");
+        staticHolder.setInitParameter("dirAllowed", "true");
+        contextHandler.addServlet(staticHolder, "/");
 
-        ServletContextHandler restContext = new ServletContextHandler();
-        ServletHolder restHolder = new ServletHolder();
-        restHolder.setInitParameter(ServerProperties.PROVIDER_PACKAGES, "org.jreactapp.api");
-        restContext.addServlet(org.glassfish.jersey.servlet.ServletContainer.class,
-                        "/api/*");
-        //restHolder.setInitOrder(0);
+        HandlerList handlers = new HandlerList();
+        handlers.addHandler(contextHandler);
+        handlers.addHandler(new DefaultHandler()); // used to show config errors
 
-        HandlerCollection handlerCollection = new HandlerCollection();
-        handlerCollection.setHandlers(new Handler[] {restContext, staticContext});
-
-        server.setHandler(handlerCollection);
+        server.setHandler(handlers);
 
         return server;
     }
